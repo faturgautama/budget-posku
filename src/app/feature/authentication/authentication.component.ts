a02fc4b91f8c24a60d8e8617b45a56b23661a6fb
Fatur Gautama <fatur@paycombat.com>
2025-07-02 21:32:32 +0700

    feat: remove supabase auth

--- src/app/feature/authentication/authentication.component.ts
+++ src/app/feature/authentication/authentication.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SupabaseService } from 'src/app/service/supabase.service';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
@@ -67,7 +66,6 @@ export class AuthenticationComponent implements OnInit {
         private _formBuilder: FormBuilder,
         private _messageService: MessageService,
         private _utilityService: UtilityService,
-        private _supabaseService: SupabaseService,
     ) {
         this.Form = this._formBuilder.group({
             email: ['', [Validators.required]],
@@ -96,20 +94,22 @@ export class AuthenticationComponent implements OnInit {
 
     onSignIn(): void {
         if (this._utilityService.checkValidator(this.Form)) {
-            this._supabaseService.checkUser(this.Form.get('email')?.value, this.Form.get('password')?.value)
-                .then((result) => {
-                    this._messageService.clear();
+            // this._supabaseService.checkUser(this.Form.get('email')?.value, this.Form.get('password')?.value)
+            //     .then((result) => {
+            //         this._messageService.clear();
 
-                    if (result[0]) {
-                        this._messageService.add({ severity: 'success', summary: 'Success', detail: 'Sign In Berhasil' });
+            //         if (result[0]) {
+            //             this._messageService.add({ severity: 'success', summary: 'Success', detail: 'Sign In Berhasil' });
 
-                        setTimeout(() => {
-                            this.handlingAuth(result[1]);
-                        }, 1500);
-                    } else {
-                        this._messageService.add({ severity: 'error', summary: 'Oops', detail: result[1] })
-                    }
-                });
+            //             setTimeout(() => {
+            //                 this.handlingAuth(result[1]);
+            //             }, 1500);
+            //         } else {
+            //             this._messageService.add({ severity: 'error', summary: 'Oops', detail: result[1] })
+            //         }
+            //     });
+
+            this.handlingAuth(null);
         }
     }
 
@@ -118,7 +118,7 @@ export class AuthenticationComponent implements OnInit {
     }
 
     private handlingAuth(data: any): void {
-        localStorage.setItem('_BGPSUD_', JSON.stringify(data));
+        localStorage.setItem('_BGPSUD_', JSON.stringify({ full_name: 'Admin', email: 'admin@unggas-berjaya.com' }));
         this._router.navigateByUrl('beranda');
     }
 }
